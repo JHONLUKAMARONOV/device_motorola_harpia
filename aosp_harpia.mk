62351b743f2e9bae6adf807235b7fdac85cbbe44
Akianonymus <anonymus.aki@gmail.com>
2019-09-20 14:00:49 +0530

    LOS >> AEX

--- aosp_harpia.mk
+++ aosp_harpia.mk
@@ -16,12 +16,11 @@
 $(call inherit-product, device/motorola/harpia/full_harpia.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_BOOT_ANIMATION_RES := 720
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
+PRODUCT_NAME := aosp_harpia
 PRODUCT_MODEL := Moto G Play
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
